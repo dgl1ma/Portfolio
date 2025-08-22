29b36beadaaf607ecd5364369ec27e004d3d54e4
Douglas Lima <douglasmybusiness@gmail.com>
2025-08-22 12:26:53 -0300

    Estilizando e terminando a aba "Projetos" - Quase pronto

--- script.js
+++ script.js
@@ -39,25 +39,3 @@ function createBubble() {
   setTimeout(() => bubble.remove(), 20000);
 }
 setInterval(createBubble, 500);
-
-// Carrossel
-
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  loop:true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 150,
-    modifier: 2.5,
-    slideShadows: true,
-  },
-  autoplay:{
-
-    delay:3000,
-    disableOnInteraction:false,
-  }
-});
